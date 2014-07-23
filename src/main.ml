965cc8cfc30cf0e46c4afacfd17f8a749c3fca07
Enguerrand Decorne <decorne.en@gmail.com>
2014-07-23 17:47:29 +0200

    Clean main.ml

--- src/main.ml
+++ src/main.ml
@@ -6,23 +6,7 @@ module Tapioca_app =
     let application_name = "tapioca"
   end)
 
-let get_document_function name =
-  Edition.get_shadowcopies ()
-  >>= fun scopies ->
-  match scopies with
-  | [] -> Lwt.return `NotConnected
-  | {id = id; text = scopy}::xs -> Lwt.return (`Result (scopy, id))
-
 let () =
-  Eliom_registration.Ocaml.register
-    ~service:Client.send_patch
-    (fun () patch ->
-       Edition.handle_patch_request patch);
-
-  Eliom_registration.Ocaml.register
-    ~service:Services.get_document
-    (fun name () -> get_document_function name);
-
   Tapioca_app.register
     ~service:Services.main_service
     (fun () () ->
